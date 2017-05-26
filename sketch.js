5d64037222f104fd7c87e9b41cdbb201390188b1
Dawid Stolarek <dawid.yerginyan@gmail.com>
2017-05-27 00:16:49 +0200

    Basic mechanics
    Added:
    - Canvas,
    - Bird object,
    - Bird sprite,
    - Basic bird movement [NEEDS FIXING].
    
    Known issues:
    - lib/Bird.js - limit bird movement to the top of the canvas /Comments in file/
    
    - sketch.js - Jumping movement needs little fine-tuning, you can work on that,
    
    - assets/css/style.css - canvas needs centering on the screens with margins/whatevers.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+var bird;
 function setup() {
-
+  createCanvas(480, 640);
+  bird = new Bird();
 }
 
 function draw() {
-  
+  background(0,0,0);
+  bird.Move();
+  bird.Render();
+}
+
+function keyPressed() {
+  if(key == ' ') {
+    // TODO:
+    // WORKING BUT YOU CAN TRY TO IMPLEMENT SMTH BETTER
+    bird.speed = -10;
+  }
 }
